34f5afc0604f4196f3a28a867241f4d277433a6e
BayLadyCoder <rachada_32@hotmail.com>
2019-01-30 17:37:51 -0500

    Added dynamic excerpt, meta box(author, date, category - all hard code),and continue reading link

--- index.php
+++ index.php
@@ -15,7 +15,15 @@
     while(have_posts()) {
       the_post(); ?>
       <div class="post-item">
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      
+        <div class="metabox">
+          <p>Posted by Bay on Jan 30, 2019 in News</p>
+        </div>
+        <div class="generic-content">
+          <?php the_excerpt(); ?>
+          <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+        </div>
       </div>
     <?php } ?>
   </div>
