1e3597fb23363de3e4130ba016130f98814c2774
BayLadyCoder <rachada_32@hotmail.com>
2019-01-29 22:14:23 -0500

    fixed a minor error

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ function learn_theme_files () {
     wp_enqueue_style('newTheme_main_style', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', learn_theme_files);
+add_action('wp_enqueue_scripts', 'learn_theme_files');
 
 
 ?>
