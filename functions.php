b8d274ba0368be3cbb77613c377974f5b5b353be
BayLadyCoder <rachada_32@hotmail.com>
2019-01-29 23:23:23 -0500

    Added dynamic title tag in browser tab

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php 
 
-// Adding style.css
+// Adding .css and .js files
 function learn_theme_files () {
     wp_enqueue_script('main-learn-theme-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -10,5 +10,12 @@ function learn_theme_files () {
 
 add_action('wp_enqueue_scripts', 'learn_theme_files');
 
+
+// Add title tag in the browser tab
+function learn_theme_features() {
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'learn_theme_features');
 
 ?>
