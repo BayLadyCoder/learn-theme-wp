e72744a148d605f260acbde50b03d1dfda89cd7b
BayLadyCoder <rachada_32@hotmail.com>
2019-01-30 00:35:57 -0500

    Notes about Wordpress functions - the_... and get_..., return or echo

---
+++ myNotes.php
@@ -0,0 +1,15 @@
+<?php
+
+// Why sometimes we have to echo/no echo the wordpress functions?
+
+the_title(); // THE... no need to use echo since the function already ECHO automatically
+get_the_title(); // GET... it RETURN the value to us, we have to manually ECHO it when we need
+
+the_ID(); // THE... no need to use echo since the function already ECHO automatically
+get_the_id(); // GET... it RETURN the value to us, we have to manually ECHO it when we need
+
+// Official Wordpress websites (Wordpress Functions)
+// https://codex.wordpress.org
+// https://developer.wordpress.org
+
+?>
