37fb47a63da7f1f6c6304922bea15f4b1e1d99b8
BayLadyCoder <rachada_32@hotmail.com>
2019-01-30 23:59:39 -0500

    Added supports excerpt to the custom post type (event) and used it

--- mu-plugins/learn-theme-post-types.php
+++ mu-plugins/learn-theme-post-types.php
@@ -7,6 +7,7 @@
 function learn_theme_post_types(){
     // https://codex.wordpress.org/Function_Reference/register_post_type
     register_post_type('event', array(
+        'supports' => array('title', 'editor', 'excerpt'), // manually add support excerpt to event editor
         'rewrite' => array( 'slug' => 'events'),
         'has_archive' => true,
         'public' => true,
